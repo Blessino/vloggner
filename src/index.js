35eb87803c89b0dbf0967fa1a1967cc9466b8c8a
Ashish Chaudhary <grunch.me1@gmail.com>
2025-08-31 15:08:10 +0545

    error and respons utils is created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import "dotenv/config";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running in port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MONGO DB connction failed", err);
+  });
